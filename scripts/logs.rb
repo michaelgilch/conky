56de377a7d8ecbf9516f74a94ab2762afd076de1
Michael Gilchrist <michaelgilch@gmail.com>
2022-01-11 04:46:26 -0500

    Minor updates to conky logs script

--- scripts/logs.rb
+++ scripts/logs.rb
@@ -8,9 +8,15 @@
 
 require 'date'
 
-rawVerboseOutput = `journalctl -p 7 -n 30 -b -o verbose --output-fields=_COMM,_TRANSPORT,SYSLOG_IDENTIFIER,PRIORITY,MESSAGE`.split("\n").to_a
+rawVerboseOutput = `journalctl --priority=7 --lines=30 --boot --output=verbose --output-fields=_COMM,_TRANSPORT,SYSLOG_IDENTIFIER,PRIORITY,_PID,MESSAGE`.split("\n").to_a
 priority = "7"
 formattedOutput = ""
+
+priority = "7"
+identifier = ""
+comm = ""
+message = ""
+pid = ""
 
 rawVerboseOutput.each do |rawLine|
     
@@ -18,44 +24,62 @@ rawVerboseOutput.each do |rawLine|
         entryDate = rawLine.split()[1]
         entryTime = rawLine.split()[2]
 
-        formattedDate = Date.parse(entryDate).strftime("%b %d")
-        formattedOutput += "${color1}" + formattedDate + " " + entryTime[0..11] + " "
+        formattedDate = Date.parse(entryDate).strftime("%d %b")
+        formattedOutput += "${color6}" + formattedDate + " "
+        formattedOutput += "${color6}" + entryTime[0..11] + " "
 
         priority = "7"
-        transport = ""
         identifier = ""
         comm = ""
         message = ""
-        color = ""
+        pid = ""
     else
         infoLine = rawLine.split("=")
 
         if infoLine[0] == "    PRIORITY"
             priority = infoLine[1]
-            #formattedOutput += priority + " "
-        elsif infoLine[0] == "    _TRANSPORT"
-            transport = infoLine[1] + ": " 
-            #formattedOutput += "${color0}" + transport  
         elsif infoLine[0] == "    SYSLOG_IDENTIFIER"
             identifier = infoLine[1] + ": "
-            formattedOutput += "${color0}" + identifier 
         elsif infoLine[0] == "    _COMM"
-            comm = infoLine[1] + ": "
-            #formattedOutput += "${color0}" + comm
+            comm = infoLine[1]
+        elsif infoLine[0] == "    _PID"
+            pid = infoLine[1]
         elsif infoLine[0] == "    MESSAGE"
             message = rawLine[12..]
-            if priority.to_i <= 3                   # emerg, alert, crit, error
-                color = "${color4}"                     # red
-            elsif priority.to_i == 4                # warning
-                color = "${color3}"                     # yellow
-            elsif priority.to_i == 5                # notice
-                color = "${color0}"                     # blue
-            elsif priority.to_i == 6                # info
-                color = "${color1}"                     # white
-            else                                    # debug
-                color = "${color2}"                     # grey
+            if priority.to_i <= 3                   # red = emerg, alert, crit, error
+                color = "${color4}"
+            elsif priority.to_i == 4                # yellow = warning
+                color = "${color3}"
+            elsif priority.to_i == 5                # grey = notice
+                color = "${color1}"
+            elsif priority.to_i == 6                # white = info
+                color = "${color0}"
+            else                                    # blue = debug
+                color = "${color2}"
             end
-            formattedOutput += color + message + "\n"
+
+            #formattedOutput += priority + " "
+            if identifier == ""
+                formattedOutput += "${color0}" + comm + "[" + pid + "]: "
+            else
+                formattedOutput += "${color0}" + identifier
+            end
+
+            # Wrap long messages
+            if message.length > 100
+                lineNum = 0
+                messageArr = message.scan(/.{1,101}/)
+                messageArr.each do |msgChunk|
+                    lineNum = lineNum + 1
+                    if lineNum == 1
+                        formattedOutput += color + msgChunk + "\n"
+                    else
+                        formattedOutput += color + "                    " + msgChunk + "\n"
+                    end
+                end
+            else
+                formattedOutput += color + message + "\n"
+            end
         end  
     end
 end
