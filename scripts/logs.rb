b7eeb7290e0c7bb165ac309129eee18f99697209
Michael Gilchrist <michaelgilch@gmail.com>
2022-01-12 20:18:12 -0500

    Fix log display to gather all data before displaying
    MESSAGE field was sometimes listed before other fields in the verbose
    output, causing the displayed entry to be missing pieces.

--- scripts/logs.rb
+++ scripts/logs.rb
@@ -8,31 +8,74 @@
 
 require 'date'
 
-rawVerboseOutput = `journalctl --priority=7 --lines=30 --boot --output=verbose --output-fields=_COMM,_TRANSPORT,SYSLOG_IDENTIFIER,PRIORITY,_PID,MESSAGE`.split("\n").to_a
-priority = "7"
-formattedOutput = ""
+journalctl = 'journalctl --boot'
+priorityArg = '--priority=7'
+numLines = '--lines=30'
+output = '--output=verbose' # Needed for color-coding
+fields = '--output-fields=_COMM,SYSLOG_IDENTIFIER,PRIORITY,_PID,MESSAGE'
+rawVerboseOutput = `#{journalctl} #{priorityArg} #{numLines} #{output} #{fields}`.split("\n").to_a
 
-priority = "7"
+# Default Values:
+formattedDate = ""
+formattedTime = ""
+priority = "7" 
 identifier = ""
 comm = ""
 message = ""
 pid = ""
+entryTime = ""
+numMsgLines = 0
+msgGroup = ""
+msgLines = ""
+formattedOutput = ""
 
+# Sample:
+# Wed 2022-01-12 19:28:45.330121 EST [s=060abe11b5924a50b4d803469c5814c5;i=2a99;b=500a9e569cfd41ebba0f8ba5db7ef3d7;m=3cbca87e1d;t=5d56bc4e852c9;x=ccf1bcfe94a82e94]
+#    _TRANSPORT=kernel
+#    PRIORITY=6
+#    SYSLOG_IDENTIFIER=kernel
+#    MESSAGE=input: SONiX USB Keyboard as /devices/pci0000:00/0000:00:14.0/usb3/3-10/3-10:1.0/0003:0C45:7681.000C/input/input43
+#Wed 2022-01-12 19:28:45.386763 EST [s=060abe11b5924a50b4d803469c5814c5;i=2a9a;b=500a9e569cfd41ebba0f8ba5db7ef3d7;m=3cbca95b5f;t=5d56bc4e9300b;x=ad0f5ab873aa43e1]
+#    _TRANSPORT=kernel
+#    PRIORITY=6
+#    SYSLOG_IDENTIFIER=kernel
+#    MESSAGE=hid-generic 0003:0C45:7681.000C: input,hidraw3: USB HID v1.11 Keyboard [SONiX USB Keyboard] on usb-0000:00:14.0-10/input0
+#Wed 2022-01-12 19:28:45.386958 EST [s=060abe11b5924a50b4d803469c5814c5;i=2a9b;b=500a9e569cfd41ebba0f8ba5db7ef3d7;m=3cbca95c22;t=5d56bc4e930ce;x=39b7875c94ddfe07]
+#    _TRANSPORT=kernel
+#    PRIORITY=6
+#    SYSLOG_IDENTIFIER=kernel
+#    MESSAGE=input: SONiX USB Keyboard Consumer Control as /devices/pci0000:00/0000:00:14.0/usb3/3-10/3-10:1.1/0003:0C45:7681.000D/input/input44
 rawVerboseOutput.each do |rawLine|
     
+    # If the line begins with something other than a space ' ' character, 
+    # we know its the start of a log block
     if rawLine[0] != " "
-        entryDate = rawLine.split()[1]
-        entryTime = rawLine.split()[2]
+        numMsgLines = numMsgLines + 1
 
-        formattedDate = Date.parse(entryDate).strftime("%d %b")
-        formattedOutput += "${color6}" + formattedDate + " "
-        formattedOutput += "${color6}" + entryTime[0..11] + " "
+        if numMsgLines > 1
+            formattedOutput += "${color6}" + formattedDate + " "
+            formattedOutput += "${color6}" + formattedTime + " "   
+            formattedOutput += msgGroup 
+            formattedOutput += msgLines
+        end
 
+        # Reset all values for next message
+        formattedDate = ""
+        formattedTime = ""
         priority = "7"
         identifier = ""
         comm = ""
         message = ""
         pid = ""
+
+        # Begin gathering new data. 
+        # Since this is a starting line of a log block, grab the date and time.
+        entryDate = rawLine.split()[1]
+        entryTime = rawLine.split()[2]
+
+        formattedDate = Date.parse(entryDate).strftime("%d %b")
+        formattedTime = entryTime[0..11]
+        
     else
         infoLine = rawLine.split("=")
 
@@ -58,11 +101,10 @@ rawVerboseOutput.each do |rawLine|
                 color = "${color2}"
             end
 
-            #formattedOutput += priority + " "
             if identifier == ""
-                formattedOutput += "${color0}" + comm + "[" + pid + "]: "
+                msgGroup = "${color0}" + comm + "[" + pid + "]: "
             else
-                formattedOutput += "${color0}" + identifier
+                msgGroup = "${color0}" + identifier
             end
 
             # Wrap long messages
@@ -72,17 +114,23 @@ rawVerboseOutput.each do |rawLine|
                 messageArr.each do |msgChunk|
                     lineNum = lineNum + 1
                     if lineNum == 1
-                        formattedOutput += color + msgChunk + "\n"
+                        msgLines = color + msgChunk + "\n"
                     else
-                        formattedOutput += color + "                    " + msgChunk + "\n"
+                        msgLines += color + "                    " + msgChunk + "\n"
                     end
                 end
             else
-                formattedOutput += color + message + "\n"
+                msgLines = color + message + "\n"
             end
         end  
     end
 end
+
+# add the last entry to the message queue
+formattedOutput += "${color6}" + formattedDate + " "
+formattedOutput += "${color6}" + entryTime[0..11] + " "   
+formattedOutput += msgGroup 
+formattedOutput += msgLines
 
 printf "${color0}Journal Logs  ${hr}\n"
 puts ""
